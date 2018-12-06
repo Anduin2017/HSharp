d69cd3f7b5ef5cf6e51bb2bb687a2d5e124f886e
Anduin2017 <anduin@aiursoft.com>
2018-12-06 12:22:09 +0800

    Fix an issue that `.` may break text tag.

--- Aiursoft.HSharp/Models/Values.cs
+++ Aiursoft.HSharp/Models/Values.cs
@@ -9,6 +9,6 @@
     {
         public static string HTMLMatch = @"<(?'TN'\w+)\s*(?'TP'[^<>]*)>(?'TC'[\d\D]*?(((?'Open'<\k'TN'[^>]*>)[\d\D]*?)+((?'-Open'</\k'TN'[^>]*>)[\d\D]*?)+)*(?(Open)(?!)))</\k'TN'>|<(?'TN'\w+)\b*(?'TP'[^<>]*)/?\s*>|<!(?'TS'[^>]+)>";
         public static string PropertiesMatch = $@"\s*([-\w]+)={"\"([^\"]*?)\""}\s*";
-        public static string NoneHTMLMatch = $@"(?'RS'\w+)";
+        public static string NoneHTMLMatch = $@"(?'RS'\w.+)";
     }
 }
