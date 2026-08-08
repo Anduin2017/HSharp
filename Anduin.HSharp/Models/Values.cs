5be59f5464e2eeb649eccc3f5cf6b5b0ce7f785d
webbrain-one <295484252+webbrain-one@users.noreply.github.com>
2026-08-08 22:08:20 +0300

    fix: prevent infinite loop in DeserializeHtml
    Parsing certain websites would cause the deserializer to hang indefinitely. This fix adjusts the logic in Values.cs to avoid dead loops.
    
    Fixes #6

--- Anduin.HSharp/Models/Values.cs
+++ Anduin.HSharp/Models/Values.cs
@@ -7,7 +7,7 @@
     }
     static class Values
     {
-        public static readonly string HtmlMatch = @"<(?'TN'\w+)\s*(?'TP'[^<>]*)>(?'TC'[\d\D]*?(((?'Open'<\k'TN'[^>]*>)[\d\D]*?)+((?'-Open'</\k'TN'[^>]*>)[\d\D]*?)+)*(?(Open)(?!)))</\k'TN'>|<(?'TN'\w+)\b*(?'TP'[^<>]*)/?\s*>|<!(?'TS'[^>]+)>";
+        public static readonly string HtmlMatch = @"<(?'TN'\w+)\s*(?'TP'[^<>]*)>(?'TC'[^<]*(((?'Open'<\k'TN'[^>]*>)[^<]*)+((?'-Open'</\k'TN'[^>]*>)[^<]*)+)*(?(Open)(?!)))</\k'TN'>|<(?'TN'\w+)\b*(?'TP'[^<>]*)/?\s*>|<!(?'TS'[^>]+)>";
         public static readonly string PropertiesMatch = @"\s*([-\w]+)=""([^""]*?)""\s*";
         public static readonly string NoneHtmlMatch = @"(?'RS'[^\r\n <]+)";
     }
